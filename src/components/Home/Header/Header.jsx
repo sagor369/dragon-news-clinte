6ce954b2c4b01a0f88da247f4ab7e1942198b7cf
Sagor <sagor.heaking@gmail.com>
2023-10-22 19:28:54 +0600

    auth provaider some change

--- src/components/Home/Header/Header.jsx
+++ src/components/Home/Header/Header.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import Marquee from "react-fast-marquee";
 import { Link } from "react-router-dom";
 import logo from "../../../assets/logo.png";
 import moment from "moment/moment";
 import { FaUser } from "react-icons/fa";
+import {user} from "Auth"
 
 const Header = () => {
+  const {user} = useContext
+  
   return (
     <div className=" relative">
       <div className="text-center pt-20 pb-4">
@@ -34,6 +37,7 @@ const Header = () => {
         </div>
         <div>
           <nav className="flex gap-4 float-right items-center">
+            
             <FaUser className="h-10 w-10 rounded-full border-black border pt-2 " />
             <Link to="/login" className="btn">
               Login
--- src/components/PrivetRoute/PriveteRoute.jsx
+++ src/components/PrivetRoute/PriveteRoute.jsx
@@ -1,9 +1,10 @@
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
-import React, { createContext } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
 import app from '../../firebase/firebase.confige';
 
 export const AuthContext = createContext()
 const auth = getAuth(app)
+const [user , setUser ]= useState()
 const display = 'sahed'
 const loginHandle = (email, password)=>{
     return createUserWithEmailAndPassword(auth, email, password)
@@ -11,10 +12,17 @@ const loginHandle = (email, password)=>{
 const registerHandle = (email, password)=>{
     return createUserWithEmailAndPassword(auth, email, password)
 }
+useEffect(()=>{
+    onAuthStateChanged(auth , users =>{
+        setUser(users)
+
+    })
+},[])
 const authInfo = {
 display,
 loginHandle,
 registerHandle,
+user
 
 }
 
